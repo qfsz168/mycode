85c9c0bdda445509d201fdd5083a68afe5633de5
WangChongquan <wangchongquan@hacfin.com>
2017-04-01 12:05:02 +0800

    tmp

---
+++ public/static/js/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by hacfin on 2017/3/31.
+ */
+/**
+ 项目JS主入口
+ 以依赖Layui的layer和form模块为例
+ **/
+layui.define(['layer', 'form'], function (exports) {
+    console.log(exports);
+    var layer = layui.layer
+        , form = layui.form();
+
+    layer.msg('Hello World');
+
+    exports('index', {}); //注意，这里是模块输出的核心，模块名必须和use时的模块名一致
+});
+
